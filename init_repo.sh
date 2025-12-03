a9d5ee3b7258b868ab74f96500304f87eeb13cb2
Hubert Nguyen <hubert@ubergizmo.com>
2025-12-03 17:42:42 -0500

    trying portsAttributes
    previous attempt failed as will, now trying with "portsAttributes" in .devcontainer.json

--- init_repo.sh
+++ init_repo.sh
@@ -5,13 +5,3 @@ if [ ! -d "vendor" ]; then
     cp .env.example .env
     php artisan key:generate
 fi
-
-
-# try opening port 80 on Github Codespaces
-if [ -z "$CODESPACE_NAME" ]; then
-    echo "Not running in a codespace, exiting."
-    exit
-fi
-
-echo "Exposing ports"
-gh codespace ports visibility 80:public -c $CODESPACE_NAME
